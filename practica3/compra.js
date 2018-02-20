7412784c56ecebbb437658f5a1aaac896acfee1f
Rodriwp <rodrigo@rodrigomompo.com>
2018-02-20 16:26:30 +0100

    parte opcional 3

--- practica3/compra.js
+++ practica3/compra.js
@@ -14,9 +14,13 @@ $ (function(){
     var cantidad = $("#cantidad").val();
     var accesorio = accesorios[seleccionado];
     precio_total = (accesorios[seleccionado].precio)*cantidad;
-    $("#total").before('<tr><td>' + accesorio.etiqueta + '</td><td>' + cantidad + '</td><td>' + precio_total.toFixed(2) + '€' + '</td></tr>');
+    if($("#"+seleccionado).length > 0){
+        $("#" + seleccionado +" td.cantidad").text((parseInt($("#" + seleccionado +" td.cantidad").text())+ parseInt(cantidad)));
+        $("#" + seleccionado +" td.subtotal").text((parseInt($("#" + seleccionado +" td.cantidad").text())*(accesorios[seleccionado].precio)).toFixed(2)+ '€')
+    }else{
+        $("#total").before('<tr id='+seleccionado+'><td>' + accesorio.etiqueta + '</td><td class="cantidad">' + cantidad + "</td><td class='subtotal'>" + precio_total.toFixed(2) + '€' + '</td></tr>');
+    }
     $("#totalval").text((parseFloat($("#totalval").text()) + precio_total).toFixed(2)) + '€';
-
 
     var tr = $("#" + referencia);
     var tdCantidad = tr.find("td:nth-child(2)");
